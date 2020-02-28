7400b48230c57d84892fcd9dfdee53dfd2026cc1
Steve <esteban.terrazas@gmail.com>
2020-02-28 06:14:23 -0800

    Realtime databases support added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,26 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/provider.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+
+
+  final productosProvider = new ProductosProvider();
 
   @override
   Widget build(BuildContext context) {
 
     final bloc = Provider.of(context);
+    
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Home')
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Email: ${bloc.email} '),
-            Divider(),
-            Text('Password: ${bloc.password}')
-          ],
+      body: _crearListado(),
+      floatingActionButton: _crearBoton(context),
+    );
+  }
+
+  Widget _crearListado(){
+
+    return FutureBuilder(
+      future: productosProvider.cargarProductos(),
+      builder: (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
+        if ( snapshot.hasData ) {
+
+          final productos = snapshot.data;
+          return ListView.builder(
+            shrinkWrap : true,
+            scrollDirection: Axis.vertical,
+            itemCount: productos.length,
+            itemBuilder: (context, i) => _crearItem(context, productos[i]),
+            );
+        } else {
+          return Center(child: CircularProgressIndicator() );
+        }
+      },
+    );
+
+  }
+
+  Widget _crearItem(BuildContext context, ProductoModel producto){
+    return  Dismissible (
+
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
         ),
+        onDismissed: (direccion){
+          // TODO: Borrar producto
+        },
+        child: ListTile(
+        title: Text('${ producto.titulo } - ${ producto.valor }'),
+        subtitle: Text(producto.id),
+        onTap: ()=> Navigator.pushNamed(context, 'producto'),
+      ),
+    );
+  }
+
+  _crearBoton(BuildContext context){
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: ()=> Navigator.pushNamed(context, 'producto'),
     );
   }
 }
